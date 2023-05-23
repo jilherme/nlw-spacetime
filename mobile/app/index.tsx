e6840550cc8b7b12fe7075d1d320f4a29b5bee9a
jilherme <rosa.guilhermediogo@gmail.com>
2023-05-22 22:42:56 -0300

    add(mobile): app layout and new memory page

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,91 +1,57 @@
-import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
-import { useRouter } from "expo-router";
-import * as SecureStore from 'expo-secure-store';
-import { StatusBar } from "expo-status-bar";
-import { styled } from "nativewind";
-import { useEffect } from "react";
-import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useRouter } from 'expo-router'
+import * as SecureStore from 'expo-secure-store'
+import { useEffect } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
 
-import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
-import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
-} from "@expo-google-fonts/roboto";
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/axios'
 
-import blurBg from "../src/assets/bg-blur.png";
-import NlwLogo from "../src/assets/nlw-spacetime-logo.svg";
-import Stripes from "../src/assets/stripes.svg";
-import { api } from "../src/lib/axios";
-
-const StyledStripes = styled(Stripes);
-
-// Endpoint
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
   tokenEndpoint: 'https://github.com/login/oauth/access_token',
-  revocationEndpoint: 'https://github.com/settings/connections/applications/bc63e0545243fbd5cbe9',
-};
-
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/d26f194cc5d5132a51be',
+}
 
 export default function App() {
   const router = useRouter()
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  });
-
-  const [, response, signWithGithub] = useAuthRequest(
+  const [, response, signInWithGithub] = useAuthRequest(
     {
-      clientId: 'bc63e0545243fbd5cbe9',
+      clientId: 'd26f194cc5d5132a51be',
       scopes: ['identity'],
       redirectUri: makeRedirectUri({
-        scheme: 'nlwspacetime'
+        scheme: 'nlwspacetime',
       }),
     },
-    discovery
-  );
+    discovery,
+  )
 
   async function handleGithubOAuthCode(code: string) {
     const response = await api.post('/register', {
-      code
+      code,
     })
 
     const { token } = response.data
 
     await SecureStore.setItemAsync('token', token)
 
-
     router.push('/memories')
   }
 
   useEffect(() => {
     if (response?.type === 'success') {
-      const { code } = response.params;
+      const { code } = response.params
 
       handleGithubOAuthCode(code)
     }
-  }, [response]);
-
-  if (!hasLoadedFonts) return null;
+  }, [response])
 
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{
-        position: "absolute",
-        left: "-100%",
-      }}
-    >
-      <StatusBar style="light" translucent />
-
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
-        <NlwLogo />
+        <NLWLogo />
 
         <View className="space-y-2">
           <Text className="text-center font-title text-2xl leading-tight text-gray-50">
@@ -100,7 +66,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
-          onPress={() => signWithGithub()}
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
@@ -111,6 +77,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-    </ImageBackground>
-  );
+    </View>
+  )
 }

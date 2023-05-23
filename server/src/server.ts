0868e82ab77b58d9500d03621c99dc8250b54a79
jilherme <rosa.guilhermediogo@gmail.com>
2023-05-23 14:16:07 -0300

    add(server): file upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,19 +4,30 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import fastify from 'fastify'
 
+import { resolve } from 'node:path'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(require('@fastify/multipart'))
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // all frontend URLs are allowed
 })
+
 app.register(jwt, {
   secret: 'spacetime',
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
